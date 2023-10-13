9166d4fc4df9ff99647f256e82e9d28a7b192521
alamon <alamon@kali.alamon>
2023-10-13 15:40:50 -0400

    Initial commit

---
+++ app/src/main/java/com/internetofhomethings/mqttdemo/Constants.java
@@ -0,0 +1,13 @@
+package com.internetofhomethings.mqttdemo;
+
+public class Constants {
+
+    public static final String MQTT_BROKER_URL = "app_startup_placeholder";
+    public static final String MQTT_CLIENT_UN = "app_startup_placeholder";
+    public static final String MQTT_CLIENT_PW = "app_startup_placeholder";
+    public static final String CLIENT_ID = "app_startup_placeholder";
+
+    public static final String CONNECTED = "Connected";
+    public static final String NOTCONNECTED = "Not Connected";
+}
+
